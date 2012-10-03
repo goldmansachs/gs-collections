71e909caaf616d8ed37546be97644a23b9ed5bec
Bhavana Hindupur <bhavana.hindupur@gs.com>
2012-10-03 03:34:07 +0000

    [GSCOLLECT-733]  Add Javadoc to generated code including the name of the template that was used to generate the code.
    git-svn-id: svn+ssh://gscollections.svn.services.gs.com/svnroot/gscollections-svn/trunk@96 d5c9223b-1aff-41ac-aadd-f810b4a99ac4

--- collections/src/main/java/com/gs/collections/impl/block/comparator/FunctionComparator.java
+++ collections/src/main/java/com/gs/collections/impl/block/comparator/FunctionComparator.java
@@ -22,7 +22,7 @@ import com.gs.collections.api.block.SerializableComparator;
 import com.gs.collections.api.block.function.Function;
 
 /**
- * Simple {@link Comparator} that uses an {@link Function}
+ * Simple {@link Comparator} that uses a {@link Function}
  * to select a value from the underlying object and compare it against a known value to determine ordering.
  */
 public class FunctionComparator<T, V>
