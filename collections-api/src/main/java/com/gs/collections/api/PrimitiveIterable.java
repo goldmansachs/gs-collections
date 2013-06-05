2f04a1df6f442484c4f7764848ab9752f181db31
Saravana Perumal K <saravana.perumalk@gs.com>
2013-06-05 05:48:00 +0000

    [GSCOLLECT-982] Add Javadoc for Primitive Iterable Interface.
    git-svn-id: svn+ssh://gscollections.svn.services.gs.com/svnroot/gscollections-svn/trunk@340 d5c9223b-1aff-41ac-aadd-f810b4a99ac4

--- collections-api/src/main/java/com/gs/collections/api/PrimitiveIterable.java
+++ collections-api/src/main/java/com/gs/collections/api/PrimitiveIterable.java
@@ -16,23 +16,84 @@
 
 package com.gs.collections.api;
 
+/**
+ * PrimitiveIterable includes API that is common to all primitive collections.
+ *
+ * @since 3.0
+ */
 public interface PrimitiveIterable
 {
+    /**
+     * Returns the number of items in this iterable.
+     *
+     * @since 3.0
+     */
     int size();
 
+    /**
+     * Returns true if this iterable has zero items.
+     *
+     * @since 3.0
+     */
     boolean isEmpty();
 
+    /**
+     * The English equivalent of !this.isEmpty()
+     *
+     * @since 3.0
+     */
     boolean notEmpty();
 
+    /**
+     * Returns a string representation of this collection by delegating to {@link #makeString(String)} and defaulting
+     * the separator parameter to the characters <tt>", "</tt> (comma and space).
+     *
+     * @return a string representation of this collection.
+     * @since 3.0
+     */
     String makeString();
 
+    /**
+     * Returns a string representation of this collection by delegating to {@link #makeString(String, String, String)}
+     * and defaulting the start and end parameters to <tt>""</tt> (the empty String).
+     *
+     * @return a string representation of this collection.
+     * @since 3.0
+     */
     String makeString(String separator);
 
+    /**
+     * Returns a string representation of this collection.  The string representation consists of a list of the
+     * collection's elements in the order they are returned by its iterator, enclosed in the start and end strings.
+     * Adjacent elements are separated by the separator string.  Elements are converted to strings using the appropriate
+     * version of <tt>String.valueOf()</tt>.
+     *
+     * @return a string representation of this collection.
+     * @since 3.0
+     */
     String makeString(String start, String separator, String end);
 
+    /**
+     * Prints a string representation of this collection onto the given {@code Appendable}.  Prints the string returned
+     * by {@link #makeString()}.
+     *
+     * @since 3.0
+     */
     void appendString(Appendable appendable);
 
+    /**
+     * Prints a string representation of this collection onto the given {@code Appendable}.  Prints the string returned
+     * by {@link #makeString(String)}.
+     *
+     * @since 3.0
+     */
     void appendString(Appendable appendable, String separator);
 
+    /**
+     * Prints a string representation of this collection onto the given {@code Appendable}.  Prints the string returned
+     * by {@link #makeString(String, String, String)}.
+     *
+     * @since 3.0
+     */
     void appendString(Appendable appendable, String start, String separator, String end);
 }
