bded88839f36a3ab91d21546fcd1017ea818db21
Bhavana Hindupur <bhavana.hindupur@gs.com>
2013-08-13 12:23:29 +0000

    [GSCOLLECT-1055] Implement immutable primitive primitive maps.
    git-svn-id: svn+ssh://gscollections.svn.services.gs.com/svnroot/gscollections-svn/trunk@372 d5c9223b-1aff-41ac-aadd-f810b4a99ac4

---
+++ collections/src/main/java/com/gs/collections/impl/block/procedure/checked/primitive/CheckedBooleanIntProcedure.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013 Goldman Sachs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gs.collections.impl.block.procedure.checked.primitive;
+
+import com.gs.collections.api.block.procedure.primitive.BooleanIntProcedure;
+
+/**
+ * This file was automatically generated from template file checkedPrimitivePrimitiveProcedure.stg.
+ *
+ * @since 4.0.
+ */
+public abstract class CheckedBooleanIntProcedure implements BooleanIntProcedure
+{
+    private static final long serialVersionUID = 1L;
+
+    public final void value(boolean item1, int item2)
+    {
+        try
+        {
+            this.safeValue(item1, item2);
+        }
+        catch (RuntimeException e)
+        {
+            throw e;
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Checked exception caught in BooleanIntProcedure", e);
+        }
+    }
+
+    @SuppressWarnings("ProhibitedExceptionDeclared")
+    public abstract void safeValue(boolean item1, int item2) throws Exception;
+}
