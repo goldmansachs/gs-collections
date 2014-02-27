074c3c32545f39c9dc12c753c98b0e5fd15ade6b
Kristen O'Leary <kristen.o'leary@gs.com>
2014-02-27 23:18:40 +0000

    [GSCOLLECT-1267] Increase code coverage in primitivePrimitivePairImpl.stg
    git-svn-id: svn+ssh://gscollections.svn.services.gs.com/svnroot/gscollections-svn/trunk@536 d5c9223b-1aff-41ac-aadd-f810b4a99ac4

---
+++ unit-tests/src/test/java/com/gs/collections/impl/tuple/primitive/BooleanObjectPairImplTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 Goldman Sachs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gs.collections.impl.tuple.primitive;
+
+import com.gs.collections.impl.test.Verify;
+import com.gs.collections.impl.tuple.Tuples;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * JUnit test for {@link BooleanBooleanPairImpl}.
+ */
+
+public class BooleanObjectPairImplTest
+{
+    @Test
+    public void testEqualsAndHashCode()
+    {
+        Verify.assertEqualsAndHashCode(PrimitiveTuples.pair(true, "false"), PrimitiveTuples.pair(true, "false"));
+        Assert.assertNotEquals(PrimitiveTuples.pair(false, "true"), PrimitiveTuples.pair(true, "false"));
+        Assert.assertEquals(Tuples.pair(true, "false").hashCode(), PrimitiveTuples.pair(true, "false").hashCode());
+    }
+
+    @Test
+    public void getOne()
+    {
+        Assert.assertTrue(PrimitiveTuples.pair(true, "false").getOne());
+        Assert.assertFalse(PrimitiveTuples.pair(false, "true").getOne());
+    }
+
+    @Test
+    public void getTwo()
+    {
+        Assert.assertEquals("true", PrimitiveTuples.pair(false, "true").getTwo());
+        Assert.assertEquals("false", PrimitiveTuples.pair(true, "false").getTwo());
+    }
+
+    @Test
+    public void testToString()
+    {
+        Assert.assertEquals("true:false", PrimitiveTuples.pair(true, "false").toString());
+        Assert.assertEquals("true:true", PrimitiveTuples.pair(true, "true").toString());
+    }
+
+    @Test
+    public void compareTo()
+    {
+        Assert.assertEquals(1, PrimitiveTuples.pair(true, "false").compareTo(PrimitiveTuples.pair(false, "false")));
+        Assert.assertEquals(0, PrimitiveTuples.pair(true, "false").compareTo(PrimitiveTuples.pair(true, "false")));
+        Assert.assertEquals(-1, PrimitiveTuples.pair(false, "false").compareTo(PrimitiveTuples.pair(true, "true")));
+    }
+}
---
+++ unit-tests/src/test/java/com/gs/collections/impl/tuple/primitive/ObjectBooleanPairImplTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 Goldman Sachs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gs.collections.impl.tuple.primitive;
+
+import com.gs.collections.impl.test.Verify;
+import com.gs.collections.impl.tuple.Tuples;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * JUnit test for {@link com.gs.collections.impl.tuple.primitive.ObjectBooleanPairImpl}.
+ */
+
+public class ObjectBooleanPairImplTest
+{
+    @Test
+    public void testEqualsAndHashCode()
+    {
+        Verify.assertEqualsAndHashCode(PrimitiveTuples.pair("true", false), PrimitiveTuples.pair("true", false));
+        Assert.assertNotEquals(PrimitiveTuples.pair("false", true), PrimitiveTuples.pair("true", false));
+        Assert.assertEquals(Tuples.pair("true", false).hashCode(), PrimitiveTuples.pair("true", false).hashCode());
+    }
+
+    @Test
+    public void getOne()
+    {
+        Assert.assertEquals("true", PrimitiveTuples.pair("true", false).getOne());
+        Assert.assertEquals("false", PrimitiveTuples.pair("false", true).getOne());
+    }
+
+    @Test
+    public void getTwo()
+    {
+        Assert.assertTrue(PrimitiveTuples.pair("false", true).getTwo());
+        Assert.assertFalse(PrimitiveTuples.pair("true", false).getTwo());
+    }
+
+    @Test
+    public void testToString()
+    {
+        Assert.assertEquals("true:false", PrimitiveTuples.pair("true", false).toString());
+        Assert.assertEquals("true:true", PrimitiveTuples.pair("true", true).toString());
+    }
+
+    @Test
+    public void compareTo()
+    {
+        Assert.assertEquals(1, PrimitiveTuples.pair("true", true).compareTo(PrimitiveTuples.pair("true", false)));
+        Assert.assertEquals(0, PrimitiveTuples.pair("true", false).compareTo(PrimitiveTuples.pair("true", false)));
+        Assert.assertEquals(-1, PrimitiveTuples.pair("true", false).compareTo(PrimitiveTuples.pair("true", true)));
+    }
+}
