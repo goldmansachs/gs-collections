26d5b92043d4dbb7ee7d29750c235181d7c9a22b
Donald Raab <donald.raab@gs.com>
2014-07-10 17:37:56 +0000

    [GSCOLLECT-1400] Add MultiReaderHashBagMultimap.
    git-svn-id: svn+ssh://gscollections.svn.services.gs.com/svnroot/gscollections-svn/trunk@637 d5c9223b-1aff-41ac-aadd-f810b4a99ac4

---
+++ collections/src/main/java/com/gs/collections/impl/multimap/bag/MultiReaderHashBagMultimap.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2014 Goldman Sachs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gs.collections.impl.multimap.bag;
+
+import java.io.Externalizable;
+
+import com.gs.collections.api.bag.ImmutableBag;
+import com.gs.collections.api.bag.MutableBag;
+import com.gs.collections.api.block.procedure.Procedure2;
+import com.gs.collections.api.map.MutableMap;
+import com.gs.collections.api.multimap.Multimap;
+import com.gs.collections.api.multimap.bag.ImmutableBagMultimap;
+import com.gs.collections.api.multimap.bag.MutableBagMultimap;
+import com.gs.collections.api.tuple.Pair;
+import com.gs.collections.impl.bag.mutable.MultiReaderHashBag;
+import com.gs.collections.impl.map.mutable.ConcurrentHashMap;
+import com.gs.collections.impl.map.mutable.UnifiedMap;
+import com.gs.collections.impl.multimap.AbstractMutableMultimap;
+
+public final class MultiReaderHashBagMultimap<K, V>
+        extends AbstractMutableMultimap<K, V, MutableBag<V>>
+        implements MutableBagMultimap<K, V>, Externalizable
+{
+    private static final long serialVersionUID = 1L;
+
+    public MultiReaderHashBagMultimap()
+    {
+    }
+
+    public MultiReaderHashBagMultimap(Multimap<? extends K, ? extends V> multimap)
+    {
+        super(Math.max(multimap.keysView().size() * 2, 16));
+        this.putAll(multimap);
+    }
+
+    public MultiReaderHashBagMultimap(Pair<K, V>... pairs)
+    {
+        super(pairs);
+    }
+
+    public MultiReaderHashBagMultimap(Iterable<Pair<K, V>> inputIterable)
+    {
+        super(inputIterable);
+    }
+
+    public static <K, V> MultiReaderHashBagMultimap<K, V> newMultimap()
+    {
+        return new MultiReaderHashBagMultimap<K, V>();
+    }
+
+    public static <K, V> MultiReaderHashBagMultimap<K, V> newMultimap(Multimap<? extends K, ? extends V> multimap)
+    {
+        return new MultiReaderHashBagMultimap<K, V>(multimap);
+    }
+
+    public static <K, V> MultiReaderHashBagMultimap<K, V> newMultimap(Pair<K, V>... pairs)
+    {
+        return new MultiReaderHashBagMultimap<K, V>(pairs);
+    }
+
+    public static <K, V> MultiReaderHashBagMultimap<K, V> newMultimap(Iterable<Pair<K, V>> inputIterable)
+    {
+        return new MultiReaderHashBagMultimap<K, V>(inputIterable);
+    }
+
+    @Override
+    protected MutableMap<K, MutableBag<V>> createMap()
+    {
+        return ConcurrentHashMap.newMap();
+    }
+
+    @Override
+    protected MutableMap<K, MutableBag<V>> createMapWithKeyCount(int keyCount)
+    {
+        return ConcurrentHashMap.newMap(keyCount);
+    }
+
+    @Override
+    protected MutableBag<V> createCollection()
+    {
+        return MultiReaderHashBag.newBag();
+    }
+
+    public MultiReaderHashBagMultimap<K, V> newEmpty()
+    {
+        return new MultiReaderHashBagMultimap<K, V>();
+    }
+
+    public MutableBagMultimap<K, V> toMutable()
+    {
+        return new MultiReaderHashBagMultimap<K, V>(this);
+    }
+
+    public ImmutableBagMultimap<K, V> toImmutable()
+    {
+        final MutableMap<K, ImmutableBag<V>> map = UnifiedMap.newMap();
+
+        this.map.forEachKeyValue(new Procedure2<K, MutableBag<V>>()
+        {
+            public void value(K key, MutableBag<V> list)
+            {
+                map.put(key, list.toImmutable());
+            }
+        });
+
+        return new ImmutableBagMultimapImpl<K, V>(map);
+    }
+}
---
+++ serialization-tests/src/test/java/com/gs/collections/impl/multimap/list/MultiReaderFastListMultimapSerializationTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2014 Goldman Sachs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gs.collections.impl.multimap.list;
+
+import com.gs.collections.api.multimap.MutableMultimap;
+import com.gs.collections.impl.multimap.MutableMultimapSerializationTestCase;
+
+public class MultiReaderFastListMultimapSerializationTest extends MutableMultimapSerializationTestCase
+{
+    @Override
+    protected MutableMultimap<String, String> createEmpty()
+    {
+        return new MultiReaderFastListMultimap<String, String>();
+    }
+
+    @Override
+    protected String getSerializedForm()
+    {
+        return "rO0ABXNyAEFjb20uZ3MuY29sbGVjdGlvbnMuaW1wbC5tdWx0aW1hcC5saXN0Lk11bHRpUmVhZGVy\n"
+                + "RmFzdExpc3RNdWx0aW1hcAAAAAAAAAABDAAAeHB3BAAAAAJ0AAFBdwQAAAADcQB+AAJ0AAFCcQB+\n"
+                + "AANxAH4AA3cEAAAAAXEAfgACeA==";
+    }
+}
