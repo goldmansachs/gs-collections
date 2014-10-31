c831b5ade858edeb1d844deac5ce346ee39a2706
Craig P. Motlin <craig.motlin@gs.com>
2014-10-31 14:13:43 +0000

    [GSCOLLECT-1454] Optimize ImmutableTreeSet.
    git-svn-id: svn+ssh://gscollections.svn.services.gs.com/svnroot/gscollections-svn/trunk@687 d5c9223b-1aff-41ac-aadd-f810b4a99ac4

--- collections/src/main/java/com/gs/collections/impl/set/sorted/immutable/ImmutableTreeSet.java
+++ collections/src/main/java/com/gs/collections/impl/set/sorted/immutable/ImmutableTreeSet.java
@@ -17,14 +17,16 @@
 package com.gs.collections.impl.set.sorted.immutable;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.SortedSet;
 
 import com.gs.collections.api.block.procedure.Procedure;
 import com.gs.collections.api.set.sorted.ImmutableSortedSet;
 import com.gs.collections.api.set.sorted.SortedSetIterable;
-import com.gs.collections.impl.UnmodifiableIteratorAdapter;
+import com.gs.collections.impl.list.mutable.FastList;
 import com.gs.collections.impl.set.sorted.mutable.TreeSortedSet;
 import net.jcip.annotations.Immutable;
 
@@ -34,44 +36,13 @@ final class ImmutableTreeSet<T>
         implements Serializable
 {
     private static final long serialVersionUID = 2L;
-    private final TreeSortedSet<T> delegate;
+    private final T[] delegate;
+    private final Comparator<? super T> comparator;
 
-    private ImmutableTreeSet(TreeSortedSet<T> delegate)
+    private ImmutableTreeSet(SortedSet<T> sortedSet)
     {
-        this.delegate = delegate;
-    }
-
-    public int size()
-    {
-        return this.delegate.size();
-    }
-
-    private Object writeReplace()
-    {
-        return new ImmutableSortedSetSerializationProxy<T>(this);
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        return this.delegate.equals(obj);
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return this.delegate.hashCode();
-    }
-
-    @Override
-    public boolean contains(Object object)
-    {
-        return this.delegate.contains(object);
-    }
-
-    public Iterator<T> iterator()
-    {
-        return new UnmodifiableIteratorAdapter<T>(this.delegate.iterator());
+        this.delegate = (T[]) sortedSet.toArray();
+        this.comparator = sortedSet.comparator();
     }
 
     public static <T> ImmutableSortedSet<T> newSetWith(T... elements)
@@ -89,6 +60,66 @@ final class ImmutableTreeSet<T>
         return new ImmutableTreeSet<T>(TreeSortedSet.newSet(set));
     }
 
+    public int size()
+    {
+        return this.delegate.length;
+    }
+
+    private Object writeReplace()
+    {
+        return new ImmutableSortedSetSerializationProxy<T>(this);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == this)
+        {
+            return true;
+        }
+
+        if (!(obj instanceof Set))
+        {
+            return false;
+        }
+        Set<?> otherSet = (Set<?>) obj;
+        if (otherSet.size() != this.size())
+        {
+            return false;
+        }
+        try
+        {
+            return this.containsAll(otherSet);
+        }
+        catch (ClassCastException ignored)
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = 0;
+        for (T each : this.delegate)
+        {
+            result += each.hashCode();
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean contains(Object object)
+    {
+        return Arrays.binarySearch(this.delegate, (T) object, this.comparator) >= 0;
+    }
+
+    public Iterator<T> iterator()
+    {
+        return FastList.newListWith(this.delegate).asUnmodifiable().iterator();
+    }
+
     public void forEach(Procedure<? super T> procedure)
     {
         this.each(procedure);
@@ -96,26 +127,54 @@ final class ImmutableTreeSet<T>
 
     public void each(Procedure<? super T> procedure)
     {
-        this.delegate.forEach(procedure);
+        for (T t : this.delegate)
+        {
+            procedure.value(t);
+        }
     }
 
     public T first()
     {
-        return this.delegate.first();
+        return this.delegate[0];
     }
 
     public T last()
     {
-        return this.delegate.last();
+        return this.delegate[this.delegate.length - 1];
     }
 
     public Comparator<? super T> comparator()
     {
-        return this.delegate.comparator();
+        return this.comparator;
     }
 
-    public int compareTo(SortedSetIterable<T> o)
+    public int compareTo(SortedSetIterable<T> otherSet)
     {
-        return this.delegate.compareTo(o);
+        Iterator<T> iterator = otherSet.iterator();
+
+        for (T eachInThis : this.delegate)
+        {
+            if (!iterator.hasNext())
+            {
+                return 1;
+            }
+
+            T eachInOther = iterator.next();
+
+            int compare = this.compare(eachInThis, eachInOther);
+            if (compare != 0)
+            {
+                return compare;
+            }
+        }
+
+        return iterator.hasNext() ? -1 : 0;
+    }
+
+    private int compare(T o1, T o2)
+    {
+        return this.comparator == null
+                ? ((Comparable<T>) o1).compareTo(o2)
+                : this.comparator.compare(o1, o2);
     }
 }
